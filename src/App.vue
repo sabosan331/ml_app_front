f12f7fc28e174d597db3b09f4fa32abe8a4faf39
sabosan331 <sabosan331@gmail.com>
2019-12-13 08:37:19 +0000

    response using axios to flask server

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,55 @@
 <template>
   <div id="ml_front">
 
-    <div class="flow_img">
-    <img alt="ml_platform" width="250px" src="./assets/ml_platform.png">
-    </div>
+  
 
     <div class="main">
     <div class="input">
-      input data
-      <ul>
-        <li>table</li>
-        <li>image</li>
-        <li>sound</li>
-        <li>time series</li>
-      </ul>
+      <p>実験条件選択</p>
+      <select v-model="selected_condition">
+          <option disabled value="">experimental condition</option>
+          <option v-for="condition in conditions" v-bind:value="condition.name" v-bind:key="condition.id">
+              {{ condition.name }}
+          </option>
+      </select>
+                <p>{{selected_condition}}</p>
+
+      
+      <p>学習，評価データ</p>
+      <button v-on:click="get_exp_data(exp_data)">get experimental information</button>
+      
+      <table align="center" style="margin : 20px auto; padding:5px; border :solid 1px #000">
+        <tr>
+          <th></th>
+          <th>ok</th>
+          <th>ng</th>
+        </tr>
+        <tr>
+          <td>学習</td>
+          <td>{{exp_data.train_ok}}</td>
+          <td>{{exp_data.train_ng}}</td>
+        </tr>
+        <tr>
+          <td>評価</td>
+          <td>{{exp_data.test_ok}}</td>
+          <td>{{exp_data.test_ng}}</td>
+        </tr>
+      </table>
+
     </div>
 
     <div class="select_method">
-      select method
+      <p>select model</p>
+      <select v-model="selected_model">
+          <option disabled value="">model</option>
+          <option v-for="model in models" v-bind:value="String(model.id) + ': '+ String(model.name)" v-bind:key="model.id ">
+              {{ model.name }}
+          </option>
+      </select>
+       <p>{{ selected_model}}</p>
       <ul>
+
+
         <li>dl or statistic ml</li>
         <li>model</li>
         <li>optimization</li>
@@ -31,7 +62,8 @@
     </div>
 
     <div class="result">
-      result
+      <p>result</p>
+
       <ul>
         <li>reration between epoch and loss</li>
         <li>confusion matrix</li>
@@ -41,6 +73,23 @@
       </ul>
     </div>
 
+    <div class="result">
+      <p>server information<br>local developing now</p>
+      
+    
+      <ul>
+        <li>front : vue (port 172.16.98.152:8080)</li>
+        <li>back  : flask (port 172.16.98.152:5000)</li>
+        <li>ml    : pytorch, scikit-learn</li>
+        <li>db    : mongodb</li>
+      </ul>
+    </div>
+
+    </div>
+
+
+    <div class="flow_img">
+      <img alt="ml_platform" width="500px" src="./assets/ml_platform.png">
     </div>
 
   </div>
@@ -48,10 +97,64 @@
 
 <script>
 
+
 export default {
   name: 'app',
+  data : function() {
+    return {
+      selected_condition: '',
+      conditions: [
+          { id: 1, name: 'train:tokushima1, test:tokushima2' },
+          { id: 2, name: 'train:tokushima1, test:tokushima1' },
+          { id: 3, name: '2 + revised by hinkan' },
+          { id: 4, name: '2 + revised by hinkan + kensaki + xc' },
+          { id: 5, name: '2 + semisupervised learning' }
+      ],
+      selected_model: '',
+      models: [
+          { id: 0, name: 'normal cnn'},
+          { id: 1, name: 'vgg16' },
+          { id: 2, name: 'resnet18' },
+          { id: 3, name: 'mobilenetv3' },
+      ],
+    counter : 0,
+    exp_data : { train_ok : 0, train_ng : 0,test_ok : 0, test_ng : 0}, // important to write like this for Object assign
+    }
+  },
+  methods : {
+    get_exp_data (exp_data) {
+      // this.axios.get('http://localhost:5000/get_exp_data/').then( res => {
+        this.axios.get('http://localhost:5000/get_exp_data/').then( res => {
+        Object.assign(exp_data,res.data )
+        console.log( exp_data ) 
+
+      });
+    },
+  }
 }
 </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <style>
 #ml_front {
@@ -74,13 +177,13 @@ body {
 }
 
 .flow_img{
-  margin-top : 1%;
+  margin-top : 100px;
   background-color:white;
 }
 
 .main {
   display: block;
-  margin-top:10px;
+  margin-top:5%;
   margin-left: auto;
   margin-right: auto;
   padding : 5px;
@@ -112,5 +215,18 @@ body {
   color : #111;
 }
 
+.server {
+   margin : 1%;
+  border : solid 1px #000;
+  color : #111;
+}
+
+th td {
+  padding : 5px;
+}
+
+
+
 
 </style>
+
